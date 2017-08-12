f295e5238e9894183efab419a82429eb08583387
Charlin Agramonte <char0394@gmail.com>
2017-08-12 12:02:21 -0400

    Sample project

--- src/Plugin.Multilingual.UWP/MultilingualImplementation.cs
+++ src/Plugin.Multilingual.UWP/MultilingualImplementation.cs
@@ -1,6 +1,7 @@
 using Plugin.Multilingual.Abstractions;
 using System;
-
+using System.Globalization;
+using System.Threading;
 
 namespace Plugin.Multilingual
 {
@@ -9,5 +10,27 @@ namespace Plugin.Multilingual
   /// </summary>
   public class MultilingualImplementation : IMultilingual
   {
-  }
+        CultureInfo _currentCultureInfo = CultureInfo.CurrentUICulture;
+        public CultureInfo CurrentCultureInfo
+        {
+            get
+            {
+                return _currentCultureInfo;
+            }
+            set
+            {
+                _currentCultureInfo = value;
+                CultureInfo.CurrentCulture = value;
+                CultureInfo.CurrentUICulture = value;
+            }
+        }
+
+        public CultureInfo DeviceCultureInfo { get { return CultureInfo.CurrentUICulture; } }
+
+        public CultureInfo[] CultureInfoList { get { return  CultureInfo.GetCultures(CultureTypes.AllCultures); } }
+
+        public CultureInfo[] NeutralCultureInfoList { get { return CultureInfo.GetCultures(CultureTypes.NeutralCultures); } }
+
+        public CultureInfo GetCultureInfo(string name) { return CultureInfo.GetCultureInfo(name); }
+    }
 }
